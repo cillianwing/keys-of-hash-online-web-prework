92b7e2280d27d717edcb7d7cff9caaf4fa2de939
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-01 11:54:07 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,8 @@
 class Hash
   def keys_of(*arguments)
     new_array = []
-    *arguments.each do |key, value|
-      if *arguments == value 
+    arguments.each do |key, value|
+      if arguments == value 
         new_array.push(key)
       end
     end
